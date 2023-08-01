7eedb1a87bb0deb0ffaa485889c46a9e48b46401
Sylar <>
2023-08-01 10:40:53 +0800

    feat: isEquals函数实现

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,47 @@
 import { isEquals } from ".";
 
-it("测试用例", () => {
-  expect(isEquals(1, 1)).toBe(true);
+it("基本类型", () => {
+  expect(isEquals(99, 99)).toBe(true);
+  expect(isEquals(99, 100)).toBe(false);
+  expect(isEquals("test", "test")).toBe(true);
+  expect(isEquals("test", "te")).toBe(false);
+});
+
+it("参数类型不同", () => {
+  expect(isEquals(1, "1")).toBe(false);
+  expect(isEquals(1, true)).toBe(false);
+  expect(isEquals(0, undefined)).toBe(false);
+  expect(isEquals(0, null)).toBe(false);
+  expect(isEquals(0, false)).toBe(false);
+  expect(isEquals("", false)).toBe(false);
+  expect(isEquals(undefined, NaN)).toBe(false);
+  expect(isEquals(null, NaN)).toBe(false);
+  expect(isEquals(null, undefined)).toBe(false);
+  expect(isEquals(null, false)).toBe(false);
+  expect(isEquals({}, [])).toBe(false);
+});
+
+it("NaN值", () => {
+  expect(isEquals(NaN, NaN)).toBe(true);
+});
+
+it("-0和+0", () => {
+  expect(isEquals(-0, +0)).toBe(true);
+  expect(isEquals(0, +0)).toBe(true);
+  expect(isEquals(-0, 0)).toBe(true);
+});
+
+it("对象类型", () => {
+  expect(
+    isEquals({ test: "666", test2: 888 }, { test: "666", test2: 888 })
+  ).toBe(true);
+  expect(
+    isEquals({ test: "66", test2: 888 }, { test: "666", test2: 888 })
+  ).toBe(false);
+  expect(isEquals({ test: "666", test2: 888 }, { test: "666" })).toBe(false);
+  expect(
+    isEquals({ test: 1, test2: [10, 20] }, { test: 1, test2: [10, 20] })
+  ).toBe(true);
+  expect(isEquals([1, 2, 3], [1, 2, 3])).toBe(true);
+  expect(isEquals([1, 2, "3"], [1, 2, 3])).toBe(false);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
+function getParamType(param: unknown) {
+  return Object.prototype.toString.call(param);
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function isEquals(a: any, b: any): boolean {
+  const typeA = getParamType(a);
+  const typeB = getParamType(b);
+  if (typeA !== typeB) {
+    return false;
+  }
+
+  if (typeA === "[object Object]" || typeA === "[object Array]") {
+    const keys1 = Object.keys(a);
+    const keys2 = Object.keys(b);
+
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+
+    for (const key of keys1) {
+      if (!isEquals(a[key], b[key])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  if (typeA === "[object Number]" && Number.isNaN(a) && Number.isNaN(b)) {
+    return true;
+  }
+
   return a === b;
 }
